79300d22e81b764facb39737037f35dd546078ab
Mahesh Vangala <vangalamaheshh@gmail.com>
2018-01-19 13:19:40 -0500

    added tornado

--- ftp/modules/apiauth.py
+++ ftp/modules/apiauth.py
@@ -3,20 +3,21 @@
 
 from pyftpdlib.authorizers import DummyAuthorizer
 import os
+import json
 from pyftpdlib._compat import unicode
-from tornado.httpclient import HTTPRequest, AsyncHTTPClient
+from tornado.httpclient import HTTPRequest, HTTPClient
 
 class APIAuth(DummyAuthorizer):
   read_perms = "el"
   write_perms = "adfmwMT"
+  auth_api = os.environ["AUTH_API"]
+  http_client = HTTPClient()
 
   def __init__(self):
     self.user_table = {}
 
   def add_user(self, username, password, homedir, perm='el',
                  msg_login="Login successful.", msg_quit="Goodbye."):
-    if self.has_user(username):
-      raise ValueError('user %r already exists' % username)
     if not isinstance(homedir, unicode):
       homedir = homedir.decode('utf8')
     if not os.path.isdir(homedir):
@@ -45,4 +46,23 @@ class APIAuth(DummyAuthorizer):
 
   def validate_authentication(self, username, password, handler):
     msg = "Authentication failed."
-    return None
+    if username and password:
+      http_req = HTTPRequest(
+        self.auth_api + "/GetUser",
+        method = "POST",
+        body = json.dumps({
+          "username": username,
+          "password": password
+        })
+      )
+      try:
+        json_data = json.loads(self.http_client.fetch(http_req).body)
+        if json_data:
+          self.add_user(username, password, "/tmp", perm = self.write_perms)
+          return None
+        else:
+          return msg
+      except:
+        return msg
+    else:
+      return msg
