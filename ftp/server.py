4f3e92300ed91c5c6d7dccb356928f84848c4088
Mahesh Vangala <vangalamaheshh@gmail.com>
2018-01-23 14:55:11 -0500

    added port range

--- ftp/server.py
+++ ftp/server.py
@@ -12,7 +12,7 @@ handler = GCPHandler
 handler.authorizer = authorizer
 handler.permit_foreign_addresses = True
 handler.masquerade_address = os.environ["PUBLIC_IP"]
-handler.passive_ports = range(60000, 60999)
+handler.passive_ports = range(60000, 60002)
 
 logging.basicConfig(filename='/var/log/pyftpd.log', level=logging.INFO)
 
